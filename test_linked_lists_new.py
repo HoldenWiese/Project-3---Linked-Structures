bb1d0bd3d3a1fcd3af152f10970db720b07bf828
Holden Wiese <wieseholden@gmail.com>
2022-10-11 21:24:41 +0000

    Finished for a 3rd time...

--- test_linked_lists_new.py
+++ test_linked_lists_new.py
@@ -66,8 +66,6 @@ def test_remove():
         cl.insert(Course(number, "test", 1.0, 2.0))
 
     course = cl.find(courseNumbers[0])
-    print(courseNumbers[0])
-    print(course)
     assert course.number() == courseNumbers[0]
     course = cl.find(courseNumbers[10])
     assert course.number() == courseNumbers[10]
@@ -85,12 +83,9 @@ def test_remove_all():
     cl.insert(Course(1000))
     for _ in range(20):
         cl.insert(Course(1200))
-    print(cl)
     cl.insert(Course(1800))
     assert len(cl) == 22
     cl.remove_all(1200)
-    print(len(cl))
-    print(cl.size())
     assert len(cl) == 2
 
 
@@ -121,7 +116,12 @@ def test_iterate_list():
 def test_code_quality():
     from pylint.lint import Run
     
-    results = Run(['courselist.py'], exit=False)
+    results = Run(['slist.py'], exit=False)
+    expected = 8.5
+    actual = results.linter.stats.global_note
+    assert actual >= expected
+
+    results = Run(['course.py'], exit=False)
     expected = 8.5
     actual = results.linter.stats.global_note
     assert actual >= expected
